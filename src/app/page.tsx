02979a9c9752c5b34e64073fe918ed6c0b9766b1
trygvea <trygve.amundsen@gmail.com>
2025-09-02 15:13:33 +0200

    Kundeliste is main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,6 @@
-import Link from "next/link"
-import { Button } from "@/components/ui/button"
+import { redirect } from "next/navigation"
 
 export default function Home() {
-  return (
-    <main className="p-8">
-      <div className="flex items-center justify-between">
-        <h1 className="text-2xl font-semibold">Kundeliste</h1>
-        <div className="space-x-2">
-          <Button asChild>
-            <Link href="/customers">Åpne kundeliste</Link>
-          </Button>
-        </div>
-      </div>
-    </main>
-  )
+	redirect("/customers")
+	return null
 }
